08dba7f64c13f6fa50226fca630b56a143d7aebb
cristian mejia <comejia@innovacion.gob.sv>
2025-01-21 14:21:43 -0600

    added script mergeSchemas.js script to merge  models into schema.prisma

---
+++ scripts/mergeSchemas.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+// Obtener la raíz del proyecto
+const rootDir = process.cwd();
+
+const prismaDir = path.join(rootDir, 'prisma');
+const modelsDir = path.join(prismaDir, 'models');
+const schemaPath = path.join(prismaDir, 'schema.prisma');
+
+const mainSchema = `
+generator client {
+  provider = "prisma-client-js"
+}
+
+datasource db {
+  provider = "postgresql"
+  url      = env("DATABASE_URL")
+}
+`;
+
+const models = fs
+  .readdirSync(modelsDir)
+  .map((file) => fs.readFileSync(path.join(modelsDir, file), 'utf-8'))
+  .join('\n\n');
+
+fs.writeFileSync(schemaPath, `${mainSchema}\n\n${models}`);
+console.log('Schema merged successfully!');
