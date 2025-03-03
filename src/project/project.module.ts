ee02d382caa26a8c858cc526fbd65320d4185c1d
cristian mejia <comejia@innovacion.gob.sv>
2025-03-03 13:22:02 -0600

    .

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import {ProjectService} from './project.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
-import { ProjectController } from './project.contoller';
+import { ProjectController } from './project.controller';
 
 
 @Module({
